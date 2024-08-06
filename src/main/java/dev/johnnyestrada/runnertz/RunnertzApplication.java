0b34c4429d92a8e5dfefcb4ea1057346638be1f7
Johnny Estrada <johnnye562@yahoo.com>
2024-08-06 12:02:09 -0700

    Initial commit

---
+++ src/main/java/dev/johnnyestrada/runnertz/RunnertzApplication.java
@@ -0,0 +1,13 @@
+package dev.johnnyestrada.runnertz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnertzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnertzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/johnnyestrada/runnertz/RunnertzApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.johnnyestrada.runnertz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnertzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
